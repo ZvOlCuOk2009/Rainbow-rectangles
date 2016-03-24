02fb96dd93673ed5cc12b7e9069837acc665efac
Zvigun Alexandr <mircamnia@ukr.net>
2016-03-24 10:57:03 +0200

    Initial Commit

---
+++ Rainbow rectangles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rainbow rectangles
+//
+//  Created by Mac on 24.03.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rainbow rectangles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rainbow rectangles
+//
+//  Created by Mac on 24.03.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
